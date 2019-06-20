0c6002aa50fbde7d9ba3b44d606dd3f1e4e4d430
bugszhou <bugszhou@outlook.com>
2019-06-20 19:01:06 +0800

    feat: Add yaml parse

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
+const path = require('path'),
+  fs = require('fs'),
+  glob = require('glob'),
+  YAML = require('yaml'),
+  cloneDeep = require('lodash/cloneDeep'),
+  merge = require('lodash/merge');
 /**
  * 环境变量支持yaml
  * @param api
  * @param projectOptions
  */
 module.exports = (api, projectOptions) => {
+  const envCwd = path.join(process.cwd(), 'config'),
+    globConfig = {
+      cwd: envCwd,
+      root: '/',
+    },
+    PRJ_ENV = process.env.PRJ_ENV || process.env.NODE_ENV || 'production',
+    defaultEnvFileGlob = glob.sync('default.*(yaml|yml)', globConfig);
+
+  if (!defaultEnvFileGlob.length) {
+    throw new Error(`config dir must include default.yml or default.yaml`);
+  }
+
+  const defaultEnv = getEnvData(defaultEnvFileGlob[0], envCwd),
+    envFile = glob.sync(`${PRJ_ENV}.*(yaml|yml)`, globConfig),
+    env = getEnvData(envFile[0], envCwd),
+    envMergeData = merge({
+      PRJ_ENV,
+    }, defaultEnv, env);
+
   api.chainWebpack(webpackConfig => {
     // 通过 webpack-chain 修改 webpack 配置
-  })
-
-  api.configureWebpack(webpackConfig => {
-    // 修改 webpack 配置
-    // 或返回通过 webpack-merge 合并的配置对象
-  })
-
-  api.registerCommand('test', args => {
-    // 注册 `vue-cli-service test`
-  })
+    webpackConfig
+      .plugin('define')
+      .tap(envData => {
+        let env = envData[0]['process.env'],
+          args = cloneDeep(envData);
+        if (!env) {
+          env = {
+            NODE_ENV: 'production',
+            BASE_URL: '/',
+          };
+        }
+        args[0]['process.env'] = {
+          ...env,
+          outData: JSON.stringify(envMergeData),
+        };
+        return args;
+      })
+  });
 };
+
+function getEnvData(url = '', envCwd = process.cwd()) {
+  if (!url) {
+    return {};
+  }
+  return YAML.parse(fs.readFileSync(path.join(envCwd, url || ''), 'utf8'));
+}
